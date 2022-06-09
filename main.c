c0d1db15e961116a14b4d1e6236659cc43dad01b
Johnny Robert <johrober@1E5.42angouleme.fr>
2022-06-09 15:09:12 +0200

    ^X not appearing anymore on signals received whatever X is

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: johrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 15:40:26 by johrober          #+#    #+#             */
-/*   Updated: 2022/06/08 17:19:09 by johrober         ###   ########.fr       */
+/*   Updated: 2022/06/09 15:07:25 by johrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,27 @@
 /* 	return (&shell); */
 /* } */
 
-int	interrupted = 0;
-
 void	receive(int signum)
 {
-	(void)signum;
-	printf("\n");
-	rl_replace_line("", 0);
 	if (signum == SIGINT)
+	{
+		printf("\n");
+		rl_replace_line("", 0);
 		rl_on_new_line();
-	rl_redisplay();
+		rl_redisplay();
+	}
 }
 
 int main()
 {
-	char	*str = NULL;
+	char			*str = NULL;
+	struct termios	termios_save;
 
 	signal(SIGINT, &receive);
 	signal(SIGQUIT, &receive);
+	tcgetattr(0, &termios_save);
+	termios_save.c_lflag &= ~ECHOCTL;
+	tcsetattr(0, 0, &termios_save);
 	str = readline(">");
 	while(str)
 	{
@@ -47,6 +50,7 @@ int main()
 			free(str);
 			str = NULL;
 		}
+		tcgetattr(0, &termios_save);
 		str = readline(">");
 	}
 }
